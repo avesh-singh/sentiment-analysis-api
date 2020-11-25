0e68edfd39005f843ebf3a513fe8ed927ed08b23
avesh-singh <aveshsingh.14157@gmail.com>
2020-11-25 22:02:51 +0530

    added bidirectional support to rnn

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, embedding_size, hidden_size, dropout, layers):
+    def __init__(self, input_size, embedding_size, hidden_size, dropout, layers, bidirectional):
         super(Encoder, self).__init__()
         self.embedding = nn.Embedding(num_embeddings=input_size, embedding_dim=embedding_size)
         self.hidden_size = hidden_size
@@ -11,7 +11,8 @@ class Encoder(nn.Module):
             self.dropout = None
         else:
             self.dropout = nn.Dropout(dropout)
-        self.rnn = nn.GRU(embedding_size, hidden_size, num_layers=layers)
+        self.bidirectional = bidirectional
+        self.rnn = nn.GRU(embedding_size, hidden_size, num_layers=layers, bidirectional=bidirectional)
 
     def forward(self, input_seq):
         embedding = self.embedding(input_seq)
@@ -24,19 +25,24 @@ class Encoder(nn.Module):
 
 class Model(nn.Module):
     def __init__(self, input_size, embedding_size, hidden_size, output_size, rnn_layers=1, dropout=0.2,
-                 enc_dropout=0.2):
+                 enc_dropout=0.2, bidirectional=True):
         super(Model, self).__init__()
-        self.encoder = Encoder(input_size, embedding_size, hidden_size, enc_dropout, rnn_layers)
+        self.encoder = Encoder(input_size, embedding_size, hidden_size, enc_dropout, rnn_layers, bidirectional)
         self.encoder_layers = rnn_layers
         self.dropout = nn.Dropout(dropout)
-        if rnn_layers > 1:
-            self.compress = nn.Linear(hidden_size * rnn_layers, hidden_size)
+        self.directions = 2 if bidirectional else 1
+        self.hidden_size = hidden_size
+        if rnn_layers > 1 or bidirectional:
+            self.compress = nn.Linear(hidden_size * rnn_layers * self.directions, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
 
     def forward(self, input_batch):
         hidden = self.encoder(input_batch)
-        if self.encoder_layers > 1:
-            concat = torch.cat([hidden[0], hidden[1]], dim=-1)
+        if self.encoder_layers > 1 or self.encoder.bidirectional:
+            hidden_layers = []
+            for i in range(self.encoder_layers * self.directions):
+                hidden_layers.append(hidden[i])
+            concat = torch.cat(hidden_layers, dim=-1)
             hidden = self.compress(concat)
         hidden = self.dropout(hidden)
         output = self.out(hidden)

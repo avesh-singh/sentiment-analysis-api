7ed5beb63206c4c0dcfeb388fe535154bb63f444
avesh-singh <aveshsingh.14157@gmail.com>
2020-11-25 20:02:59 +0530

    added metrics for testing

--- constants.py
+++ constants.py
@@ -4,12 +4,12 @@ torch.manual_seed(1)
 torch.backends.cudnn.deterministic = True
 FILENAME = 'data/airline_sentiment_analysis.csv'
 PROCESSED_FILENAME = 'data/airline_sentiment_analysis_processed.csv'
-MAX_SENTENCE_LEN = 15
+MAX_SENTENCE_LEN = 20
 EMBEDDING_SIZE = 200
 HIDDEN_SIZE = 32
-N_LAYERS = 1
+N_LAYERS = 2
 BATCH_SIZE = 32
 LIN_DROPOUT = 0.2
-EPOCHS = 8
+EPOCHS = 10
 VOCAB_SIZE = 5000
 device = "cuda" if torch.cuda.is_available() else "cpu"
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from model import *
 from data import prepare_data_keras
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, confusion_matrix
 from constants import *
 
 
@@ -25,6 +25,7 @@ def train_iters():
     best_valid_score = 0
     for e in range(EPOCHS):
         train_loss = 0
+        train_acc = 0
         train_tgt = []
         train_pred = []
         for i, batch in enumerate(train_buck):
@@ -32,26 +33,32 @@ def train_iters():
             train_loss += loss
             train_tgt.extend(train_target)
             train_pred.extend(train_prediction)
-        valid_loss, expected, prediction = evaluate()
-        valid_score = f1_score(expected, prediction)
+        valid_loss, expected, prediction = evaluate(valid_buck)
+        valid_score = accuracy_score(expected, prediction)
         if best_valid_score < valid_score:
             print("new best model! improvement: %f" % (valid_score - best_valid_score))
             best_valid_score = valid_score
             torch.save(model.state_dict(), 'model.pt')
-        print(f"epochs: {e} | training precision: {precision_score(train_tgt, train_pred):.4f} | training recall:"
-              f" {recall_score(train_tgt, train_pred):.4f} | training loss: {train_loss / len(train_buck):.4f} |"
-              f" validation precision: {precision_score(expected, prediction):.4f} | validation recall: "
+        print(f"epochs: {e} | train acc: {accuracy_score(train_tgt, train_pred):.4f} | training precision:"
+              f" {precision_score(train_tgt, train_pred):.4f} | "
+              f"training "
+              f"recall:"
+              f" {recall_score(train_tgt, train_pred):.4f} | training loss: {train_loss / len(train_buck):.4f}")
+        print(f"validation acc: {accuracy_score(expected, prediction):.4f} | validation precision:"
+              f" {precision_score(expected, prediction):.4f} | validation "
+              f"recall: "
               f"{recall_score(expected, prediction):.4f} | "
               f"validation score: {f1_score(expected, prediction):.4f} | "
               f"validation loss: {valid_loss:.4f}\n")
+        print()
 
 
-def evaluate():
+def evaluate(iterator):
     with torch.no_grad():
         all_targets = []
         all_preds = []
         loss = 0
-        for b in valid_buck:
+        for b in iterator:
             label = b[1].view(-1, 1).to(dtype=torch.float)
             all_targets.extend(list(label.squeeze().cpu().numpy()))
             text = b[0]
@@ -64,3 +71,13 @@ def evaluate():
 
 if __name__ == '__main__':
     train_iters()
+    model.load_state_dict(torch.load('model.pt'))
+    model.eval()
+    loss, tgts, preds = evaluate(test_buck)
+    print(confusion_matrix(tgts, preds))
+    print(f"test acc: {accuracy_score(tgts, preds):.4f} | test precision:"
+          f" {precision_score(tgts, preds):.4f} | test "
+          f"recall: "
+          f"{recall_score(tgts, preds):.4f} | "
+          f"test score: {f1_score(tgts, preds):.4f} | "
+          f"test loss: {loss:.4f}\n")
